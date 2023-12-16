419ca0aae0def57b469f35c0600aef99b08c505e
snaksa <sinan.zikri@gmail.com>
2023-12-16 12:38:27 +0200

    Add cloudfront additional domain and certificate

--- backend/bin/devbg-scraper.ts
+++ backend/bin/devbg-scraper.ts
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 import "source-map-support/register";
+import * as dotenv from "dotenv";
+import * as path from "path";
 import * as cdk from "aws-cdk-lib";
 import { ScraperStack } from "../lib/stack/scraper-stack/scraper-stack";
 import { StorageStack } from "../lib/stack/storage-stack/storage-stack";
 import { ApiStack } from "../lib/stack/api-stack/api-stack";
 import { FrontendStack } from "../lib/stack/frontend-stack/frontend-stack";
 
+dotenv.configDotenv({ path: path.resolve(__dirname, "../.env") });
+
 const app = new cdk.App();
+
 const storageStack = new StorageStack(app, "DevbgStorageStack");
 
 const scraperStack = new ScraperStack(app, "DevbgScraperStack", {
--- backend/lib/stack/frontend-stack/frontend-stack.ts
+++ backend/lib/stack/frontend-stack/frontend-stack.ts
@@ -2,8 +2,13 @@ import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Bucket, BucketAccessControl } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
-import { Distribution, OriginAccessIdentity } from "aws-cdk-lib/aws-cloudfront";
+import {
+  Distribution,
+  OriginAccessIdentity,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
 import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 
 export class FrontendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -23,8 +28,15 @@ export class FrontendStack extends Stack {
 
     new Distribution(this, "DevBgScraperFrontendDistribution", {
       defaultRootObject: "index.html",
+      domainNames: ["sinilinx.com"],
+      certificate: Certificate.fromCertificateArn(
+        this,
+        "DevBgScraperFrontendCertificate",
+        process.env.CERTIFICATE_ARN ?? "",
+      ),
       defaultBehavior: {
         origin: new S3Origin(websiteBucket, { originAccessIdentity }),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
     });
 
