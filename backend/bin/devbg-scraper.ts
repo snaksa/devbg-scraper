0d0067e8e004b5e96b4c712697ba28eec629b0f1
snaksa <sinan.zikri@gmail.com>
2024-01-14 15:14:47 +0200

    Test CDK synth

--- backend/bin/devbg-scraper.ts
+++ backend/bin/devbg-scraper.ts
@@ -10,18 +10,27 @@ import { FrontendStack } from "../lib/stack/frontend-stack/frontend-stack";
 
 dotenv.configDotenv({ path: path.resolve(__dirname, "../.env") });
 
+const account = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: "us-east-1",
+};
+
 const app = new cdk.App();
 
-const storageStack = new StorageStack(app, "DevbgStorageStack");
+const storageStack = new StorageStack(app, "DevbgStorageStack", {
+  env: account,
+});
 
 const scraperStack = new ScraperStack(app, "DevbgScraperStack", {
   dbStore: storageStack.dbStore,
+  env: account,
 });
 scraperStack.addDependency(storageStack);
 
 const apiStack = new ApiStack(app, "DevbgApiStack", {
   dbStore: storageStack.dbStore,
+  env: account,
 });
 apiStack.addDependency(storageStack);
 
-new FrontendStack(app, "DevbgFrontendStack");
+new FrontendStack(app, "DevbgFrontendStack", { env: account });
